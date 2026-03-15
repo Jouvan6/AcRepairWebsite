ccd94ed6f6b303765d24488dd6575e24e8c279e7
Jouvana Gerges <116806407+Jouvan6@users.noreply.github.com>
2026-03-15 10:29:03 +0200

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AcRepairWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcRepairWebsite
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
+
